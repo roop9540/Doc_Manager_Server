70b008684047e0986022a04b0c48627351faca71
Roop <roop9540@gmail.com>
2023-04-12 02:15:02 +0530

    added

--- index.js
+++ index.js
@@ -14,15 +14,28 @@ require("./routes/user/passport")
 
 //==================== MiddleWares ====================
 const app = express();
-app.use(cors());
+
 // app.use(cookieParser)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use("/uploads", express.static(__dirname + "/uploads"));
+app.use(cors());
 // console.log(__dirname + "/uploads")
 
 
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method == "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+  
+    next();
+  });
 
 
 app.get("/", (req, res) => {
