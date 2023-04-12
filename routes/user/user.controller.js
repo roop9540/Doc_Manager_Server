41eaa679673d84290dd8b56ff4233965cc3b5bb3
Roop <roop9540@gmail.com>
2023-04-12 12:55:55 +0530

    add

--- routes/user/user.controller.js
+++ routes/user/user.controller.js
@@ -10,7 +10,22 @@ const passport = require('passport');
 
  const JWT_SECRET = "mysecretkey";
  
-
+ const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
 
 
 async function register(req, res) {
@@ -149,4 +164,11 @@ async function protected(req, res) {
     }
      }
 
+     allowCors(register);
+     allowCors(login);
+     allowCors(profile);
+
     module.exports = { register, login, protected, profile }
+
+
+
