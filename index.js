d3aef773aadcee5f0d5056a22d59f81ff4f2c084
Roop <roop9540@gmail.com>
2023-04-12 12:22:01 +0530

    add

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use("/uploads", express.static(__dirname + "/uploads"));
-// app.use(cors());
+app.use(cors());
 // console.log(__dirname + "/uploads")
 
 
